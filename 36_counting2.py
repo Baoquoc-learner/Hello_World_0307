cee56c37713c61fb4b4fe004e11200cbda51a0c3
Baoquoc-learner <baoquoctran9a2@gmail.com>
2025-07-08 08:19:07 +0700

    V1.0.25.0708
    --Update users comfirm...

--- 36_counting2.py
+++ 36_counting2.py
@@ -1,9 +1,12 @@
 # Using countinue in a loop
 
 currentNumber = 0
-while currentNumber < 20:
+while currentNumber < 10:
     currentNumber += 1
     if currentNumber % 2 == 0:
+        # break  # break sẽ kết thúc ngay lập tức
+        # continue  # chuyển sang giá trị tiếp theo của vòng lập
         continue
+        # print(currentNumber)
 
     print(currentNumber)
